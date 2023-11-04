c6152f3e9b8838fa2eaea4b69fa1a3458194ffab
bvdreddine <badrbourrazq@gmail.com>
2023-11-04 05:41:39 -0700

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for digit in range(0, 100):
-    if digit == 99:
-        print("{}".format(digit))
+for i  in range(0, 100):
+    if i == 99:
+        print("{}".format(i))
     else:
-        print("{:02}".format(digit), end=', ')
+        print("{:02}".format(i), end=', ')
